5ee8ab2eb60d3ce2e443167ff2d2288b7491187e
Aliaksei Urbanski <aliaksei.urbanski@gmail.com>
2026-07-06 14:04:45 +0300

    ♻️ Use async_sessionmaker

--- src/core/deps/db.py
+++ src/core/deps/db.py
@@ -1,19 +1,19 @@
 from collections.abc import AsyncGenerator
-from typing import Annotated, cast
+from typing import Annotated
 
 from fastapi import Depends, Request
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 
 async def get_session(request: Request) -> AsyncGenerator[AsyncSession]:
-    async_session = sessionmaker(
+    async_session = async_sessionmaker(
         request.app.state.db,
         class_=AsyncSession,
         expire_on_commit=False,
+        autoflush=False,
     )
-    session = cast(AsyncSession, async_session())
-    async with session:
+    async with async_session() as session:
         yield session
 
 
